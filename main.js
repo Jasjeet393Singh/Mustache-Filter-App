7efacbaeeeaa30b19858b05a7f362930b4f17713
Jasjeet393Singh <81762166+Jasjeet393Singh@users.noreply.github.com>
2021-09-11 18:52:43 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,35 +1,40 @@
-function preload() {
+noseX=0;
+noseY=0;
 
+function preload() {
+  mustache = loadImage('https://pngimg.com/uploads/moustache/moustache_PNG38.png');
 }
 
 function setup() {
-    canvas = createCanvas(640,480);
-    canvas.center();
-    video = createCapture(VIDEO);
-    video.size(640,480);
-    video.hide();
+  canvas = createCanvas(640, 480);
+  canvas.center();
+  video = createCapture(VIDEO);
+  video.size(640, 480);
+  video.hide();
 
-    poseNet = ml5.poseNet(video, modelLoaded);
-    poseNet.on('Pose', gotPoses);
+  poseNet = ml5.poseNet(video, modelLoaded);
+  poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded() {
+  console.log('PoseNet Is Initialized');
+}
+
+function gotPoses(results)
+{
+  if(results.length > 0)
+  {
+    console.log(results);
+    mustacheX = results[0].pose.mustache.x-15;
+    mustacheY = results[0].pose.mustache.y-15;
+  }
 }
 
 function draw() {
-    image(video, 0, 0, 640, 480);
+  image(video, 0, 0, 640, 480);
+  image(mustache, noseX, noseY, 30, 30);
 }
 
-
-function modelLoaded() {
-    console.log('PoseNet is Initialised')
-}
-
-function gotPoses(results) {
-    if(results.length > 0) {
-        console.log(results);
-        console.log("mustache x = " + results[0].pose.mustache.x);
-        console.log("mustache y = " + results[0].pose.mustache.y);
-    }
-}
-
-function saveImage() {
-    save('myMustacheFilterImage.png');
+function take_snapshot(){    
+  save('myFilterImage.png');
 }
